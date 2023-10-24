e9eca1ff9de7af2d69cfb3bf331744621cee2bf4
Sardorbee <sardorgoyibnazarov@gmail.com>
2023-10-24 13:43:52 +0500

    added assets

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,8 @@
+class AppImages{
+  static const String bgImage = "assets/image/bg_image.png";
+  static const String bgDetail = "assets/image/bg_detail.png";
+  static const String earth = "assets/image/earth.png";
+  static const String mars = "assets/image/mars.png";
+  static const String mercury = "assets/image/mercury.png";
+  static const String venus = "assets/image/venus.png";
+}
